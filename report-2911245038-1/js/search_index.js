047457546e4e104172d9cd62057c64e27d602225
N0-man <nouman.memon@icloud.com>
2022-08-23 17:23:40 +0530

    Deploy Reports

---
+++ report-2911245038-1/js/search_index.js
@@ -0,0 +1,1 @@
+var index = {"Tags":{"clinic":["specs/labLite.html","specs/paymentLite.html"],"lab-lite":["specs/labLite.html"],"payment-lite":["specs/paymentLite.html"]},"Specs":{"Lab Lite":["specs/labLite.html"],"Payment Lite":["specs/paymentLite.html"]}};
