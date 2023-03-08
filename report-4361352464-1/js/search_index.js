fbc8052ca92da9e2dbc12eb05353e96e40141a42
bahmni-infra <infrastructure@bahmni.org>
2023-03-08 10:53:22 +0530

    Deploy Reports

---
+++ report-4361352464-1/js/search_index.js
@@ -0,0 +1,1 @@
+var index = {"Tags":{"clinic":["specs/labLite.html","specs/paymentLite.html"],"lab-lite":["specs/labLite.html"],"payment-lite":["specs/paymentLite.html"],"regression":["specs/labLite.html","specs/paymentLite.html"],"smoke":["specs/labLite.html","specs/paymentLite.html"]},"Specs":{"Lab Lite":["specs/labLite.html"],"Payment Lite":["specs/paymentLite.html"]}};
