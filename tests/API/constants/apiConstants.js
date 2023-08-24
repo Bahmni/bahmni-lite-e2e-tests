691399984e2f4992dd93f40dd99bdcf5b52b90ec
priyanshu1602 <48365227+priyanshu1602@users.noreply.github.com>
2023-08-24 20:00:04 +0530

    Automation script for CDSS (#20)
    * Automation script for CDSS
    
    * fixed form builder isssue

--- tests/API/constants/apiConstants.js
+++ tests/API/constants/apiConstants.js
@@ -15,7 +15,8 @@ const endpoints = {
     DIAGNOSIS_SEARCH:"/openmrs/ws/rest/v1/concept",
     SNOWSTORM_URL:"https://snowstorm.snomed.mybahmni.in/fhir/ValueSet/$expand",
     ECL_QUERY:"http://snomed.info/sct?fhir_vs=ecl/<<",
-    VALUESET_URL:"http://bahmni.org/fhir/ValueSet/bahmni-valueset-hospitalisation"
+    VALUESET_URL:"http://bahmni.org/fhir/ValueSet/bahmni-valueset-hospitalisation",
+    CDSS_ENABLE_URL:"/openmrs/ws/rest/v1/bahmnicore/sql/globalproperty?property=cdss.enable"
 }
 
 const expectedValues = {
