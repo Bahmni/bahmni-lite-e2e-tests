809570ae5d519ede975c7faf6dc37773940cd791
gokultw <gokul.kuppan@thoughtworks.com>
2023-11-21 18:16:34 +0530

    fix wait for env

--- .github/waitForEnv.sh
+++ .github/waitForEnv.sh
@@ -5,13 +5,17 @@ max_timeout=900
 max_timeout_temp=$max_timeout
 retry_time=10
 url="https://${TARGET_ENV}.mybahmni.in/openmrs/"
-if [[ "$TARGET_ENV" == "demo" || "$TARGET_ENV" == "docker.standard" ]]; then
+if grep -q "$TARGET_ENV" <<< "demo"; then
         flag=true
         echo "ENV_FLAG=true" >> $GITHUB_ENV
         echo "Skipping environment check for ${TARGET_ENV} instance"
         exit 0
-else
-         echo "TARGET_ENV is not 'demo' or 'docker.standard'. Actual value: $TARGET_ENV"
+fi
+if grep -q "$TARGET_ENV" <<< "docker.standard"; then
+        flag=true
+        echo "ENV_FLAG=true" >> $GITHUB_ENV
+        echo "Skipping environment check for ${TARGET_ENV} instance"
+        exit 0
 fi
 while [ $max_timeout -gt 0 ]
 do
